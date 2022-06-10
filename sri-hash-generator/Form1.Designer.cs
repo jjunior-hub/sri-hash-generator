d1efce6afe4d861686ecbf085eafa7a4056ec4d9
jjunior-hub <joselito.junior@viavarejo.com.br>
2022-06-10 13:12:17 -0300

    Add project files.

---
+++ sri-hash-generator/Form1.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace sri_hash_generator
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.script = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.selecionarArquivo = new System.Windows.Forms.Button();
+            this.btnExportar = new System.Windows.Forms.Button();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(51, 104);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 62;
+            this.dataGridView1.RowTemplate.Height = 28;
+            this.dataGridView1.Size = new System.Drawing.Size(489, 210);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // script
+            // 
+            this.script.HeaderText = "script";
+            this.script.MinimumWidth = 8;
+            this.script.Name = "script";
+            this.script.Width = 150;
+            // 
+            // selecionarArquivo
+            // 
+            this.selecionarArquivo.Location = new System.Drawing.Point(51, 26);
+            this.selecionarArquivo.Name = "selecionarArquivo";
+            this.selecionarArquivo.Size = new System.Drawing.Size(188, 34);
+            this.selecionarArquivo.TabIndex = 1;
+            this.selecionarArquivo.Text = "Selecionar Arquivo";
+            this.selecionarArquivo.UseVisualStyleBackColor = true;
+            this.selecionarArquivo.Click += new System.EventHandler(this.selecionarArquivo_Click);
+            // 
+            // btnExportar
+            // 
+            this.btnExportar.Location = new System.Drawing.Point(615, 104);
+            this.btnExportar.Name = "btnExportar";
+            this.btnExportar.Size = new System.Drawing.Size(112, 34);
+            this.btnExportar.TabIndex = 2;
+            this.btnExportar.Text = "Exportar >>";
+            this.btnExportar.UseVisualStyleBackColor = true;
+            this.btnExportar.Click += new System.EventHandler(this.btnExportar_Click);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(51, 353);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(361, 34);
+            this.progressBar1.TabIndex = 3;
+            this.progressBar1.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.btnExportar);
+            this.Controls.Add(this.selecionarArquivo);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "sri-hash-generator";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn script;
+        private System.Windows.Forms.Button selecionarArquivo;
+        private System.Windows.Forms.Button btnExportar;
+        private System.Windows.Forms.ProgressBar progressBar1;
+    }
+}
---
+++ sri-hash-generator/Form1.cs
@@ -0,0 +1,264 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace sri_hash_generator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void selecionarArquivo_Click(object sender, EventArgs e)
+        {
+
+
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+
+            openFileDialog1.InitialDirectory = "c:\\";
+            openFileDialog1.Filter = "CSV files (*.csv)|*.csv|Excel Files|*.xls;*.xlsx";
+            openFileDialog1.FilterIndex = 2;
+            openFileDialog1.RestoreDirectory = true;
+
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+
+
+                    this.progressBar1.Visible = true;
+                    this.progressBar1.Minimum = 1;
+                    this.progressBar1.Maximum = 4;
+                    this.progressBar1.Value = 2;
+                    this.progressBar1.Step = 1;
+
+
+
+
+                    this.progressBar1.PerformStep();
+                    var dataTable = ConvertToDataTable(openFileDialog1.FileName);
+
+
+                    if (!dataTable.Columns.Contains("HashSHA256"))
+                        dataTable.Columns.Add("HashSHA256");
+
+                    if (!dataTable.Columns.Contains("HashSHA384"))
+                        dataTable.Columns.Add("HashSHA384");
+
+                    if (!dataTable.Columns.Contains("HashSHA512"))
+                        dataTable.Columns.Add("HashSHA512");
+
+
+
+                    this.progressBar1.PerformStep();
+                    for (int i = 0; i < dataTable.Rows.Count; i++)
+                    {
+
+                        var script = dataTable.Rows[i]["Script"].ToString();
+
+                        if (!string.IsNullOrEmpty(script))
+                        {
+
+
+                            try
+                            {
+                                using (var lWebClient = new WebClient())
+                                {
+
+                                    lWebClient.Headers.Add("Accept", "*/*");
+                                    lWebClient.Headers.Add("Accept-Encoding", "gzip, deflate, br");
+
+                                    using (var fs = new MemoryStream(lWebClient.DownloadData(new Uri(script))))
+                                    {
+                                        dataTable.Rows[i]["HashSHA256"] = HashGeneratorSHA256(fs);
+                                        dataTable.Rows[i]["HashSHA384"] = HashGeneratorSHA384(fs);
+                                        dataTable.Rows[i]["HashSHA512"] = HashGeneratorSHA512(fs);
+                                    }
+                                }
+
+                            }
+                            catch (WebException exlWebClient)
+                            {
+                                dataTable.Rows[i]["HashSHA256"] = exlWebClient.Message;
+                                dataTable.Rows[i]["HashSHA384"] = exlWebClient.Message;
+                                dataTable.Rows[i]["HashSHA512"] = exlWebClient.Message;
+                            }
+
+                        }
+                    }
+
+
+                    this.dataGridView1.DataSource = dataTable;
+                    this.progressBar1.PerformStep();
+
+
+                }
+                catch (Exception ex)
+                {
+
+                }
+            }
+
+        }
+
+
+        public string HashGeneratorSHA256(MemoryStream fs)
+        {
+
+
+            using (SHA256 sha256Hash = SHA256.Create())
+            {
+                return "sha256-" + Convert.ToBase64String(sha256Hash.ComputeHash(fs));
+            }
+
+
+        }
+
+
+
+        public string HashGeneratorSHA384(MemoryStream fs)
+        {
+
+            using (SHA384 sha384Hash = SHA384.Create())
+            {
+                return "sha384-" + Convert.ToBase64String(sha384Hash.ComputeHash(fs));
+            }
+
+
+        }
+
+
+        public string HashGeneratorSHA512(MemoryStream fs)
+        {
+
+            using (SHA512 sha512Hash = SHA512.Create())
+            {
+                return "sha512-" + Convert.ToBase64String(sha512Hash.ComputeHash(fs));
+            }
+
+        }
+
+
+
+        public System.Data.DataTable ConvertToDataTable(string path)
+        {
+
+            System.Data.DataTable dt = null;
+
+            try
+
+            {
+
+                object rowIndex = 1;
+
+                dt = new System.Data.DataTable();
+
+                DataRow row;
+
+                Microsoft.Office.Interop.Excel.Application app = new Microsoft.Office.Interop.Excel.Application();
+
+                Microsoft.Office.Interop.Excel.Workbook workBook = app.Workbooks.Open(path, 0, true, 5, "", "", true,
+
+                Microsoft.Office.Interop.Excel.XlPlatform.xlWindows, "\t", false, false, 0, true, 1, 0);
+
+                Microsoft.Office.Interop.Excel.Worksheet workSheet = (Microsoft.Office.Interop.Excel.Worksheet)workBook.ActiveSheet;
+
+                int temp = 1;
+
+                while (((Microsoft.Office.Interop.Excel.Range)workSheet.Cells[rowIndex, temp]).Value2 != null)
+
+                {
+
+                    dt.Columns.Add(Convert.ToString(((Microsoft.Office.Interop.Excel.Range)workSheet.Cells[rowIndex, temp]).Value2));
+
+                    temp++;
+
+                }
+
+                rowIndex = Convert.ToInt32(rowIndex) + 1;
+
+                int columnCount = temp;
+
+                temp = 1;
+
+                while (((Microsoft.Office.Interop.Excel.Range)workSheet.Cells[rowIndex, temp]).Value2 != null)
+
+                {
+
+                    row = dt.NewRow();
+
+                    for (int i = 1; i < columnCount; i++)
+
+                    {
+
+                        row[i - 1] = Convert.ToString(((Microsoft.Office.Interop.Excel.Range)workSheet.Cells[rowIndex, i]).Value2);
+
+                    }
+
+                    dt.Rows.Add(row);
+
+                    rowIndex = Convert.ToInt32(rowIndex) + 1;
+
+                    temp = 1;
+
+                }
+
+                app.Workbooks.Close();
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+            return dt;
+
+        }
+
+        private void btnExportar_Click(object sender, EventArgs e)
+        {
+            Microsoft.Office.Interop.Excel.Application XcelApp = new Microsoft.Office.Interop.Excel.Application();
+
+            if (this.dataGridView1.Rows.Count > 0)
+            {
+                try
+                {
+                    XcelApp.Application.Workbooks.Add(Type.Missing);
+                    for (int i = 1; i < this.dataGridView1.Columns.Count + 1; i++)
+                    {
+                        XcelApp.Cells[1, i] = this.dataGridView1.Columns[i - 1].HeaderText;
+                    }
+                    //
+                    for (int i = 0; i < this.dataGridView1.Rows.Count - 1; i++)
+                    {
+                        for (int j = 0; j < this.dataGridView1.Columns.Count; j++)
+                        {
+                            XcelApp.Cells[i + 2, j + 1] = this.dataGridView1.Rows[i].Cells[j].Value.ToString();
+                        }
+                    }
+                    //
+                    XcelApp.Columns.AutoFit();
+                    //
+                    XcelApp.Visible = true;
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Erro : " + ex.Message);
+                    XcelApp.Quit();
+                }
+            }
+        }
+    }
+}
